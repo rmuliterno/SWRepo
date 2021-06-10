b4281411741657b72e9d4e0d3319d1779994a240
rmuliterno <renan-mf1@hotmail.com>
2021-06-10 17:23:04 -0300

    feat: initial home page draft

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Home: React.FC = () => (
+  <Container>
+    <h1>Star wars appp</h1>
+  </Container>
+);
+
+export default Home;
