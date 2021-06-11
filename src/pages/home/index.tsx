f7db4be92be3324c7aa344bb00c924a45370f90e
rmuliterno <renan-mf1@hotmail.com>
2021-06-11 20:11:01 -0300

    feat: improving layout (responsive grid, loading animation) and adding useCallback hook

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,39 +1,86 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
+import { ClipLoader } from 'react-spinners';
 
+import AnimationContainer from '../../components/AnimationContainer';
 import api from '../../services/api';
-import { Container } from './styles';
+
+import { Container, Title, Info, LoadingContainer, MoviesGrid, MovieContainer, MovieTitle, Button } from './styles';
 
 interface FilmProps {
   title: string,
   release_date: string,
+  episode_id: number,
 }
 
 const Home: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [films, setFilms] = useState<FilmProps[]>([]);
 
-  useEffect(() => {
-    const getFilms = async () => {
+  const getFilms = async () => {
+    try {
+      setLoading(true);
+      setError(false);
       const response = await api.get('/films');
 
-      console.log(response.data);
       setFilms(response.data.results);
+      setLoading(false);
+    } catch(err) {
+      console.log(err);
+      setLoading(false);
+      setError(true);
     }
+  }
 
+  useEffect(() => {
     getFilms();
   }, []);
 
-  return (
-    <Container>
-      <h1>Home</h1>
-      <div>
+  const handleClick = useCallback((film: FilmProps) => {
+    alert(`${film.title} was released in ${film.release_date}`)
+  }, []);
+
+  const renderMovies = useCallback(() => {
+    if (loading) {
+      return (
+        <LoadingContainer>
+          <ClipLoader />
+        </LoadingContainer>
+      )
+    }
+
+    if (error) {
+      return (
+        <AnimationContainer>
+          <Info>No movie found :(</Info>
+          <Button onClick={getFilms}>Try again!</Button>
+        </AnimationContainer>
+      )
+    }
+
+    if (films.length <= 0 && !loading) {
+      return <Info>No movie found :(</Info>
+    }
+
+    return (
+      <MoviesGrid>
         {films &&
           films.map(film => (
-            <>
-              <p>{film.title}</p>
-              <button onClick={() => alert(film.release_date)}>{'Ver lan\u00e7amento'}</button>
-            </>
+            <MovieContainer key={film.episode_id}>
+              <MovieTitle>{film.title}</MovieTitle>
+              <Button onClick={() => handleClick(film)}>More info</Button>
+            </MovieContainer>
         ))}
-      </div>
+      </MoviesGrid>
+    )
+  }, [error, films, handleClick, loading]);
+
+  return (
+    <Container>
+      <Title>Star Wars Movies</Title>
+      <AnimationContainer>
+        {renderMovies()}
+      </AnimationContainer>
     </Container>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,5 +1,57 @@
 import styled from 'styled-components';
+import { fadeIn } from '../../components/AnimationContainer/styles';
 
 export const Container = styled.div`
   height: 100%;
+`;
+
+export const LoadingContainer = styled.div`
+  height: 100%;
+  margin-top: 32px;
+`;
+
+export const Title = styled.h1`
+  color: var(--textColor);
+  margin-bottom: 32px;
+`;
+
+export const Info = styled.h3`
+  color: var(--textColor);
+  margin-bottom: 16px;
+  font-weight: 400;
+`;
+
+export const MoviesGrid = styled.div`
+  animation: ${fadeIn} 1s;
+  max-width: 1200px;
+  margin: 0 auto;
+  display: grid;
+  grid-gap: 1rem;
+
+  @media (min-width: 600px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (min-width: 900px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
+
+export const MovieContainer = styled.div`
+  margin: 14px 0;
+`;
+
+export const MovieTitle = styled.h3`
+  color: var(--textColor);
+  font-weight: 400;
+`;
+
+export const Button = styled.button`
+  background-color: #0368b9;
+  border: none;
+  color: #eee;
+  padding: 8px 20px;
+  text-align: center;
+  margin: 4px 0;
+  font-size: 16px;
 `;
