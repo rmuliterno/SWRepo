aab6fff122dfc7ae0fb7d746578852e2dacaa2ec
rmuliterno <renan-mf1@hotmail.com>
2021-06-11 18:17:19 -0300

    feat: initial api request done

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
+import api from '../../services/api';
 import { Container } from './styles';
 
-const Home: React.FC = () => (
-  <Container>
-    <h1>Star wars appp</h1>
-  </Container>
-);
+interface FilmProps {
+  title: string,
+  release_date: string,
+}
+
+const Home: React.FC = () => {
+  const [films, setFilms] = useState<FilmProps[]>([]);
+
+  useEffect(() => {
+    const getFilms = async () => {
+      const response = await api.get('/films');
+
+      console.log(response.data);
+      setFilms(response.data.results);
+    }
+
+    getFilms();
+  }, []);
+
+  return (
+    <Container>
+      <h1>Home</h1>
+      <div>
+        {films &&
+          films.map(film => (
+            <>
+              <p>{film.title}</p>
+              <button onClick={() => alert(film.release_date)}>{'Ver lan\u00e7amento'}</button>
+            </>
+        ))}
+      </div>
+    </Container>
+  )
+}
 
 export default Home;
